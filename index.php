6cff54b5d537a69edfd24d2447346c63f7379868
Miguelon <aguirrevarelam@gmail.com>
2018-07-22 19:25:46 +0200

    Con un te quiero incluido

--- index.php
+++ index.php
@@ -2,10 +2,17 @@
 <html>
     <head>
         <meta charset="utf-8">
+        <link rel="stylesheet" href="estilos/estilos_principal.css">
         <title>Darkness</title>
     </head>
     <body>
-<?php
-?>        
+        <header id="cabecera">Cabecera de la página</header>
+        <div id="centro">
+            <div id="sidebar">Links a otras páginas</div>
+            <div id="content"> Contenido de la página</div>
+        </div>
+        <div id="pie">
+            Aquí va el pie con el copyright de la empresa
+        </div>
     </body>
 </html>
