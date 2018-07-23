02d1a157ab083c991cbc7a333e46ee30d908cc00
VirginiaOR <ortizrvirginia@gmail.com>
2018-07-23 16:15:33 +0200

    no consigo ponerlos en el sitio adecuado, debajo de enlaces

--- index.php
+++ index.php
@@ -9,8 +9,23 @@
         <header id="cabecera">Cabecera de la página</header>
         <div id="centro">
             <div id="sidebar">Links a otras páginas</div>
-            <div id="content"> Contenido de la página</div>
-        </div>
+             
+            <a href="link_darkness/Bibliofrafia.php">Bibliografía
+            </a></br>
+             <a href="link_darkness/Form_Registro.php">Regístrate
+             </a></br>
+            <a href="link_darkness/Form_login.php">Loguéate
+            </a></br>
+            <a href="link_darkness/Usuarios.php">Usuarios Registrados
+            </a></br>
+            
+            
+            
+            
+            
+          <div id="content"> Contenido de la página</div>
+         </div>   
+          
         <div id="pie">
             Aquí va el pie con el copyright de la empresa
         </div>
